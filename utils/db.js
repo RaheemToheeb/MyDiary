ff630f875a72bee106a691a1901599f1e63d7281
RaheemToheeb <raheemtoheeb87@gmail.com>
2022-06-09 18:54:00 +0100

    first commit

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const url = process.env.URL;
+// const url = process.env.URL;
+const atlas = process.env.ATLAS;
 
-mongoose.connect(url).then(() => {
-	console.log("database is now connected...!");
+mongoose.connect(atlas).then(() => {
+  console.log("database is now connected...!");
 });
 
 module.exports = mongoose;
